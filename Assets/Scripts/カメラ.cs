af22b9c0e3879ee78bad09a002be2dcda0b01281
Hirosaway <TTC2550SG0042@edu.tech.ac.jp>
2025-07-17 12:01:48 +0900

    7.17
    カメラの追従処理　
    ジャンプの処理の追加

---
+++ Assets/Scripts/カメラ.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollwCamera : MonoBehaviour
+{
+    //カメラの位置をプレイヤーに合わせて変更する
+    Vector3 CameraPosition = Vector3.zero;
+
+    public GameObject followTarget;
+    public float followSpeed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //カメラと追従したいオブジェクトの間の距離を取得する
+        CameraPosition = followTarget.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            followTarget.transform.position - CameraPosition,
+            Time.deltaTime * followSpeed
+            );
+    }
+}
